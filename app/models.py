a0c14fe6888d2e275b2621e032240b7e23b18c22
dcortes <dcortes@campus.udes.edu.co>
2018-10-10 15:46:54 -0500

    Init models

--- app/models.py
+++ app/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Quotation(models.Model):
+    pass
+
+
+class Stock(models.Model):
+    ref = models.TextField()
+    created_at = models.DateField()
+
+
+class StockRawMaterial(models.Model):
+    date_raw_material_in = models.DateField(null=True, blank=True)
+    caliber = models.DecimalField(default=0.0)
